f01ba438058f6e7fedab546a85a6e4b39dd97706
bitc0der <59016822+bitc0der@users.noreply.github.com>
2024-10-19 15:22:52 +0700

    Minor refactoring

--- src/StreamTools.Benchmarks/Utils/CompressExtensions.cs
+++ src/StreamTools.Benchmarks/Utils/CompressExtensions.cs
@@ -1,16 +1,17 @@
 ﻿using System;
 using System.IO.Compression;
 using System.IO;
+using System.Text;
 
 namespace StreamTools.Benchmarks.Utils;
 
 internal static class CompressExtensions
 {
-	public static byte[] Compress(this string source)
+	public static byte[] Compress(this string source, Encoding? encoding = null)
 	{
 		ArgumentNullException.ThrowIfNull(source);
 
-		using var inputStream = new StringStream(source: source);
+		using var inputStream = new StringStream(source: source, encoding);
 		using var outputStream = new MemoryStream();
 		using var gZipStream = new GZipStream(outputStream, CompressionMode.Compress, leaveOpen: true);
 
@@ -22,6 +23,7 @@ internal static class CompressExtensions
 	public static void DecompressTo(this byte[] data, Stream output)
 	{
 		ArgumentNullException.ThrowIfNull(data);
+		ArgumentNullException.ThrowIfNull(output);
 
 		using var sourceStream = new MemoryStream(data);
 		using var gZipStream = new GZipStream(sourceStream, CompressionMode.Decompress, leaveOpen: true);
