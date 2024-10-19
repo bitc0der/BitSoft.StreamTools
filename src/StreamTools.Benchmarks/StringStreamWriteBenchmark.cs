13344944f0e8deae2e6f73fa2475b151dbc230e7
bitc0der <59016822+bitc0der@users.noreply.github.com>
2024-10-19 15:20:27 +0700

    Extract compression utils

--- src/StreamTools.Benchmarks/StringStreamWriteBenchmark.cs
+++ src/StreamTools.Benchmarks/StringStreamWriteBenchmark.cs
@@ -1,7 +1,6 @@
 ﻿using BenchmarkDotNet.Attributes;
 using StreamTools.Benchmarks.Utils;
 using System.IO;
-using System.IO.Compression;
 using System.Text;
 
 namespace StreamTools.Benchmarks;
@@ -15,14 +14,14 @@ public class StringStreamWriteBenchmark
 	[GlobalSetup]
 	public void GlobalSetup()
 	{
-		_buffer = Compress(Create.String(length: 64 * 1024 * 1024));
+		_buffer = Create.String(length: 64 * 1024 * 1024).Compress();
 	}
 
 	[Benchmark]
 	public string StrinbStream()
 	{
 		using var stream = new StringStream();
-		Decompress(output: stream, data: _buffer!);
+		_buffer!.DecompressTo(stream);
 		return stream.GetString();
 	}
 
@@ -30,7 +29,7 @@ public class StringStreamWriteBenchmark
 	public string StrinbStream_ArrayPool()
 	{
 		using var stream = new StringStream(() => new ArrayStringBuffer());
-		Decompress(output: stream, data: _buffer!);
+		_buffer!.DecompressTo(stream);
 		return stream.GetString();
 	}
 
@@ -38,7 +37,7 @@ public class StringStreamWriteBenchmark
 	public string StrinbStream_MemoeryPool()
 	{
 		using var stream = new StringStream(() => new MemoryStringBuffer());
-		Decompress(output: stream, data: _buffer!);
+		_buffer!.DecompressTo(stream);
 		return stream.GetString();
 	}
 
@@ -46,25 +45,7 @@ public class StringStreamWriteBenchmark
 	public string MemoryStream()
 	{
 		using var stream = new MemoryStream();
-		Decompress(output: stream, data: _buffer!);
+		_buffer!.DecompressTo(stream);
 		return Encoding.UTF8.GetString(stream.GetBuffer());
 	}
-
-	private static byte[] Compress(string source)
-	{
-		using var inputStream = new StringStream(source: source);
-		using var outputStream = new MemoryStream();
-		using var gZipStream = new GZipStream(outputStream, CompressionMode.Compress, leaveOpen: true);
-
-		inputStream.CopyTo(gZipStream);
-
-		return outputStream.GetBuffer();
-	}
-
-	private static void Decompress(Stream output, byte[] data)
-	{
-		using var sourceStream = new MemoryStream(data);
-		using var gZipStream = new GZipStream(sourceStream, CompressionMode.Decompress, leaveOpen: true);
-		gZipStream.CopyTo(output);
-	}	
 }
---
+++ src/StreamTools.Benchmarks/Utils/CompressExtensions.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO.Compression;
+using System.IO;
+
+namespace StreamTools.Benchmarks.Utils;
+
+internal static class CompressExtensions
+{
+	public static byte[] Compress(this string source)
+	{
+		ArgumentNullException.ThrowIfNull(source);
+
+		using var inputStream = new StringStream(source: source);
+		using var outputStream = new MemoryStream();
+		using var gZipStream = new GZipStream(outputStream, CompressionMode.Compress, leaveOpen: true);
+
+		inputStream.CopyTo(gZipStream);
+
+		return outputStream.GetBuffer();
+	}
+
+	public static void DecompressTo(this byte[] data, Stream output)
+	{
+		ArgumentNullException.ThrowIfNull(data);
+
+		using var sourceStream = new MemoryStream(data);
+		using var gZipStream = new GZipStream(sourceStream, CompressionMode.Decompress, leaveOpen: true);
+		gZipStream.CopyTo(output);
+	}
+}
